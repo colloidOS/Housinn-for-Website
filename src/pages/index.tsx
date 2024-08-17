eb13eaad324e5c1452b58985be1695c06f6c5c55
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-17 11:41:10 +0100

    Initialize project with app router

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Footer from "@/components/Footer";
-
-export default function Home() {
-  return (
-    <main className={`flex flex-col items-center font-sans`}>
-      <Footer />
-    </main>
-  );
-}
