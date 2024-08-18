878a1a10363d608f686a67cc3cde36d064ec6dfc
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-18 12:49:41 +0100

    made some ammends

---
+++ src/components/OurServices.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+const WhyChooseUs: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <h1>Why Choose Us?</h1>
+        <p>Get the best real estate services here at Housinn</p>
+      </div>
+
+      <div className="grid grid-cols-2">
+        <div>
+          {" "}
+          <div>
+            icon{" "}
+            <div>
+              <h2>Diverse Property Inventory</h2>
+              <p>
+                Housinn offers a wide variety of properties for our clients to
+                choose from. If you're looking for a cozy home, a modern
+                apartment, a spacious house, or a commercial space, we have the
+                perfect property for you.
+              </p>
+            </div>
+          </div>
+          <div>
+            icon{" "}
+            <div>
+              <h2>Local Expertise</h2>
+              <p>
+                Learn about local real estate markets from our team. We provide
+                valuable insights into neighborhood dynamics, market trends, and
+                potential investment opportunities, ensuring that you make
+                informed decisions.
+              </p>
+            </div>
+          </div>{" "}
+          <div>
+            icon{" "}
+            <div>
+              <h2>User-Friendly Platform</h2>
+              <p>
+                Housinn's user-friendly platform makes real estate easy. Our
+                intuitive search features, easy-to-use interface, and
+                streamlined processes empower users to find, shortlist, and
+                connect with properties easily. Your property search has never
+                been more accessible.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          {" "}
+          <div>
+            icon{" "}
+            <div>
+              <h2>Trustworthy Services</h2>
+              <p>
+                Housinn offers a wide variety of properties for our clients to
+                choose from. If you're looking for a cozy home, a modern
+                apartment, a spacious house, or a commercial space, we have the
+                perfect property for you.
+              </p>
+            </div>
+          </div>
+          <div>
+            icon{" "}
+            <div>
+              <h2>Exceptional Customer Service</h2>
+              <p>
+              Our team of real estate professionals offers personalized and attentive service to all, from first-time buyers to seasoned investors, ensuring your satisfaction throughout the process and addressing any concerns you may have.
+              </p>
+            </div>
+          </div>{" "}
+          <div>
+            icon{" "}
+            <div>
+              <h2>User-Friendly Platform</h2>
+              <p>
+                Housinn's user-friendly platform makes real estate easy. Our
+                intuitive search features, easy-to-use interface, and
+                streamlined processes empower users to find, shortlist, and
+                connect with properties easily. Your property search has never
+                been more accessible.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyChooseUs;
