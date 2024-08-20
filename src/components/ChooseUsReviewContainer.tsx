e880ee7edad71def084bc62e68ed5f14c56ebbcb
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-20 21:25:25 +0100

    I completed whychooseus and reviews

---
+++ src/components/ChooseUsReviewContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WhyChooseUs from "./WhyChooseUs";
+import Reviews from "./Reviews";
+
+const ChooseUsReviewContainer = () => {
+  return (
+    <section className="relative h-full bg-contain bg-center bg-repeat" style={{ backgroundImage: "url('/images/chooseus&review.png')" }}>
+      <WhyChooseUs />
+      <Reviews />
+    </section>
+  );
+};
+
+export default ChooseUsReviewContainer;
