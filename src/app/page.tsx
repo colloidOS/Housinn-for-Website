cc6a539d89a964e15b81dfa940f268161cc1d3df
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-20 14:53:14 +0100

    I finalized the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ import AboutUs from "../components/aboutus/page";
 import Services from "../components/services/page";
 import HeroContainer from "../components/HeroSectionContainer";
 import WhyChooseUs from "@/components/OurServices";
+import GetOurApp from "@/components/GetOurApp";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col ">
+    <main className="flex min-h-screen flex-col w-full ">
       <HeroContainer />
       <AboutUs />
       <Services />
       <WhyChooseUs />
+      <GetOurApp/>
       <Footer />
     </main>
   );
