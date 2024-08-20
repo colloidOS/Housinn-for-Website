c24fd6d80013dda9e151331808d3af4909881f99
Nwanne-san <113237530+Nwanne-san@users.noreply.github.com>
2024-08-20 23:36:34 +0100

    created dashboard directory, implement sidebar

---
+++ src/app/dashboard/messages/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Messages() {
+  return (
+    <div className='w-full bg-background-2 px-12 py-10 '>Messages</div>
+  )
+}
+
+export default Messages
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='w-full bg-background-2 px-12 py-10 '>Dashboard</div>
+  )
+}
+
+export default Dashboard
