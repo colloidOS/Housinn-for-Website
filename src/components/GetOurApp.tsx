46538cb34576b01c7017d2d2b8bf8271b913013d
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-21 18:16:43 +0100

    I updated getourapp

--- src/components/GetOurApp.tsx
+++ src/components/GetOurApp.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import GoogleDownloadButton from "../../public/icons/google-bownload-button.svg";
 import AppleDownloadButton from "../../public/icons/apple-download-button.svg";
+import LeftPhone from "../../public/icons/left-phone.svg";
+import RightPhone from "../../public/icons/right-phone.svg";
 const GetOurApp: React.FC = () => {
   return (
     <div className="grid grid-cols-2 get-our-app mx-[104px] rounded-[36px] text-white pl-[89px] pt-[76px] pb-[63px] mb-[286px] mt-[68px]">
@@ -16,19 +18,33 @@ const GetOurApp: React.FC = () => {
           {" "}
           <Image
             src={GoogleDownloadButton}
-            width={180}
+            width={140}
             height={52}
             alt="Google-download-button"
           />
           <Image
             src={AppleDownloadButton}
-            width={180}
+            width={140}
             height={52}
             alt="Apple-download-button"
           />
         </div>
       </div>
-      <div className="col-span-1"></div>
+      <div className="col-span-1">
+        {" "}
+        <Image
+          src={LeftPhone}
+          width={140}
+          height={52}
+          alt="left-phone"
+        />{" "}
+        <Image
+          src={RightPhone}
+          width={140}
+          height={52}
+          alt="right-phone"
+        />
+      </div>
     </div>
   );
 };
