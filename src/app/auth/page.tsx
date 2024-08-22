c040db313e78858af8684b1b70d795dee839a132
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-08-22 19:20:51 +0100

    I finished the auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import SignInWithApple from "../../../public/icons/sign-in-with-apple.svg";
-import SignInWithGoogle from "../../../public/icons/sign-in-with-google.svg";
+import Apple from "../../../public/icons/apple.svg";
+import Google from "../../../public/icons/google.svg";
 
 const AuthPage = () => {
   const [isSignIn, setIsSignIn] = useState(true);
@@ -75,21 +75,20 @@ const AuthPage = () => {
   ];
 
   return (
-    <div className="flex w-full">
+    <div className="relative flex w-full ">
       <div
-        className="w-1/2 bg-contain bg-no-repeat bg-center"
+        className="sticky top-0 hidden w-1/2 h-screen bg-center bg-no-repeat bg-cover md:flex"
         style={{
           backgroundImage: `url('/images/sign-in.png')`,
         }}
       ></div>
 
-      {/* Right side with form */}
-      <div className="w-1/2 flex flex-col justify-center items-center">
-        <div className="text-center flex flex-col gap-7 px-[114px] pt-9">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/2">
+        <div className="flex flex-col gap-7 px-[65px] pt-[62px] pb-[140px] text-center md:px-[114px] md:pt-9">
           <h1 className="text-2xl font-bold">Welcome To Housinn</h1>
-          <div className="flex justify-center">
+          <div className="flex justify-center w-full px-[13px]">
             <button
-              className={`px-6 border-b-2 text-[14px] py-2 focus:outline-none ${
+              className={`flex-1 px-6 py-2 text-[14px] border-b-2 focus:outline-none ${
                 isSignIn ? "border-primary" : "border-white-300"
               }`}
               onClick={() => setIsSignIn(true)}
@@ -97,7 +96,7 @@ const AuthPage = () => {
               Sign in
             </button>
             <button
-              className={`px-6 border-b-2 text-[14px] py-2 focus:outline-none ${
+              className={`px-6 py-2 text-[14px] border-b-2 focus:outline-none ${
                 !isSignIn ? "border-primary" : "border-white-300"
               }`}
               onClick={() => setIsSignIn(false)}
@@ -106,7 +105,7 @@ const AuthPage = () => {
             </button>
           </div>
           {isSignIn ? (
-            <form className="">
+            <form className="px-[13px]">
               {signInFields.map((field) => (
                 <div key={field.id} className="mb-3 text-left">
                   <label
@@ -119,43 +118,49 @@ const AuthPage = () => {
                     id={field.id}
                     type={field.type}
                     placeholder={field.placeholder}
-                    className="w-full px-4 py-2 border border-white-300 rounded focus:outline-none placeholder:text-[14px]"
+                    className="w-full px-2 py-2 placeholder:text-[14px] border border-white-300 rounded focus:outline-none"
                   />
                 </div>
               ))}
-              <button className="w-full mt-8 mb-4 bg-primary text-base text-white py-2 rounded">
+              <button className="w-full py-2 mt-8 mb-4 text-base text-white bg-primary rounded">
                 Sign in
               </button>
-              <div className="text-center">
-                <a href="#" className="text-primary text-[14px]">
+              <div className="mb-2 text-center">
+                <a href="#" className="text-[14px] text-primary">
                   Forgot your password?
                 </a>
               </div>
-              <div className="flex justify-center text-white-300 items-center gap-2">
-                <div className="border-[1px] border-white-300 flex-1"></div>
-                or <div className="border-[1px] border-white-300 flex-1"></div>
+              <div className="flex items-center justify-center gap-2 text-white-300">
+                <div className="flex-1 border-[1px] border-white-300"></div>
+                or <div className="flex-1 border-[1px] border-white-300"></div>
               </div>
               <div className="flex flex-col w-full gap-[14px] pt-3">
-                <Image
-                  src={SignInWithApple}
-                  width={200}
-                  height={20}
-                  alt="Sign in with Apple"
-                  className="w-full"
-                />
-                <Image
-                  src={SignInWithGoogle}
-                  width={200}
-                  height={10}
-                  alt="Sign in with Google"
-                  className="w-full"
-                />
+                <div className="flex items-center justify-between px-[7.6px] py-[6px] pr-[30px] border-[1px] rounded-[5px]">
+                  <Image
+                    src={Google}
+                    width={30}
+                    height={30}
+                    alt="Sign in with Google"
+                  />
+                  <p className="text-[14px] font-semibold">
+                    Sign in with Google
+                  </p>
+                </div>
+                <div className="flex items-center justify-between px-[7.6px] py-[6px] pr-[34px] border-[1px] rounded-[5px]">
+                  <Image
+                    src={Apple}
+                    width={30}
+                    height={30}
+                    alt="Sign in with Apple"
+                  />
+                  <p className="text-[14px] font-semibold">Sign in with Apple</p>
+                </div>
               </div>
             </form>
           ) : (
-            <form className="flex flex-col gap-3 text-left">
+            <form className="flex flex-col gap-3 px-[13px] text-left">
               {newAccountFields.map((field) => (
-                <div key={field.id} className="">
+                <div key={field.id}>
                   <label
                     htmlFor={field.id}
                     className="text-[14px] font-semibold"
@@ -166,52 +171,63 @@ const AuthPage = () => {
                     id={field.id}
                     type={field.type}
                     placeholder={field.placeholder}
-                    className="w-full px-4 py-2 border border-white-300 rounded focus:outline-none placeholder:text-[14px]"
+                    className="w-full px-[10px] py-2 placeholder:text-[14px] border border-white-300 rounded focus:outline-none"
                   />
                 </div>
               ))}
               <div className="pt-3">
                 <p className="text-[14px] font-semibold">Account Type</p>
-                <div className="grid grid-cols-2 grid-rows-2 justify-around mt-2">
+                <div className="grid grid-cols-2 grid-rows-2 mt-2">
                   {accountTypes.map((type) => (
-                    <label key={type.id} className="text-[13px] ">
+                    <label
+                      key={type.id}
+                      className="flex items-center gap-1 text-[11px]"
+                    >
                       <input
                         type="radio"
                         id={type.id}
                         name="accountType"
                         value={type.value}
-                      />{" "}
+                      />
                       {type.label}
                     </label>
                   ))}
                 </div>
-                <p className="text-[10px] p-2 my-3">
-                  By submitting, I accept Housinn’s Terms of Use and Privacy
-                  Policy{" "}
+                <p className="p-2 my-3 text-[12px] text-center">
+                  By submitting, I accept Housinn’s{" "}
+                  <span className="font-semibold text-primary">
+                    Terms of Use and Privacy
+                  </span>
                 </p>
               </div>
-              <button className="w-full bg-primary text-white py-2 rounded">
+              <button className="w-full py-2 text-white bg-primary rounded">
                 Create Account
               </button>
-              <div className="flex justify-center text-white-300 items-center gap-2">
-                <div className="border-[1px] border-white-300 flex-1"></div>
-                or <div className="border-[1px] border-white-300 flex-1"></div>
+              <div className="flex items-center justify-center gap-2 text-white-300">
+                <div className="flex-1 border-[1px] border-white-300"></div>
+                or <div className="flex-1 border-[1px] border-white-300"></div>
               </div>
               <div className="flex flex-col w-full gap-[14px] pt-3">
-                <Image
-                  src={SignInWithApple}
-                  width={200}
-                  height={20}
-                  alt="Sign in with Apple"
-                  className="w-full"
-                />
-                <Image
-                  src={SignInWithGoogle}
-                  width={200}
-                  height={10}
-                  alt="Sign in with Google"
-                  className="w-full"
-                />
+                <div className="flex items-center justify-between px-[7.6px] py-[6px] pr-[30px] border-[1px] rounded-[5px]">
+                  <Image
+                    src={Google}
+                    width={30}
+                    height={30}
+                    alt="Sign in with Google"
+                  />
+                  <p className="text-[14px] font-semibold">
+                    Sign in with Google
+                  </p>
+                </div>
+                <div className="flex items-center justify-between px-[7.6px] py-[6px] pr-[34px] border-[1px] rounded-[5px]">
+                  <Image
+                    src={Apple}
+                    width={30}
+                    height={30}
+                    alt="Sign in with Apple"
+                  />
+                  <p className="text-[14px] font-semibold">Sign in with Apple</p>
+                </div>
               </div>
             </form>
           )}
