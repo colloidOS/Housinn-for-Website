18c3b2c1e5e57d96a441ea0689759dddd4567b58
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-11-18 12:32:00 +0100

    fix: updated the confirm password

--- src/data/auth.ts
+++ src/data/auth.ts
@@ -1,62 +1,70 @@
 export const signInFields = [
-    {
-      id: "signin-email",
-      name: "email",
-      type: "email",
-      placeholder: "Enter your email",
-      label: "Email",
-    },
-    {
-      id: "signin-password",
-      name: "password",
-      type: "password",
-      placeholder: "Enter your password",
-      label: "Password",
-    },
-  ];
+  {
+    id: "signin-email",
+    name: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+  },
+  {
+    id: "signin-password",
+    name: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+  },
+];
 
-  export const newAccountFields = [
-    {
-      id: "first-name",
-      name: "firstName",
-      type: "text",
-      placeholder: "Enter your first name",
-      label: "First name",
-    },
-    {
-      id: "last-name",
-      name: "lastName",
-      type: "text",
-      placeholder: "Enter your last name",
-      label: "Last name",
-    },
-    {
-      id: "signup-email",
-      name: "email",
-      type: "email",
-      placeholder: "Enter your email",
-      label: "Email",
-    },
-    {
-      id: "signup-password",
-      name: "password",
-      type: "password",
-      placeholder: "Create password",
-      label: "Password",
-    },
-  ];
+export const newAccountFields = [
+  {
+    id: "first-name",
+    name: "firstName",
+    type: "text",
+    placeholder: "Enter your first name",
+    label: "First name",
+  },
+  {
+    id: "last-name",
+    name: "lastName",
+    type: "text",
+    placeholder: "Enter your last name",
+    label: "Last name",
+  },
+  {
+    id: "signup-email",
+    name: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+  },
+  {
+    id: "signup-password",
+    name: "password",
+    type: "password",
+    placeholder: "Create password",
+    label: "Password",
+  },
 
-  export const accountTypes = [
-    { id: "account-type-individual", value: "Individual", label: "Individual" },
-    {
-      id: "account-type-property-owner",
-      value: "Property Owner",
-      label: "Property Owner",
-    },
-    { id: "account-type-agent", value: "Agent", label: "Agent" },
-    {
-      id: "account-type-property-developer",
-      value: "Property Developer",
-      label: "Property Developer",
-    },
-  ];
+  {
+    id: "confirm-password",
+    name: "confirmPassword",
+    type: "password",
+    placeholder: "Confirm your password",
+    label: "Confirm Password",
+  },
+];
+
+export const accountTypes = [
+  { id: "account-type-individual", value: "Individual", label: "Individual" },
+  {
+    id: "account-type-property-owner",
+    value: "Property Owner",
+    label: "Property Owner",
+  },
+  { id: "account-type-agent", value: "Agent", label: "Agent" },
+  {
+    id: "account-type-property-developer",
+    value: "Property Developer",
+    label: "Property Developer",
+  },
+];
